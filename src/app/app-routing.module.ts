c9a97b83e1ed3200ad5e59d2711f80964cd4526e
TilekSydykov <tilek.sydykov@iaau.edu.kg>
2021-01-31 14:45:59 +0600

    v0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+import {UsersComponent} from "./components/users/users.component";
+import {TerminalsComponent} from "./components/terminals/terminals.component";
+import {TerminalComponent} from "./components/terminal/terminal.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'terminals', component: TerminalsComponent},
+  {path: 'terminal/:id', component: TerminalComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { UsersComponent } from './components/users/users.component';
+import { TerminalsComponent } from './components/terminals/terminals.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UsersComponent,
+    TerminalsComponent,
+    TerminalComponent,
+    PageNotFoundComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
